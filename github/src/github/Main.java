c1c787ff3f142dd39c495d356b5c1a7ef65c031a
sdac <sdac@sdac-pc>
2023-05-23 19:01:46 +0530

    main code

---
+++ github/src/github/Main.java
@@ -0,0 +1,17 @@
+package github;
+class Sum{
+	public int sum(int x,int y){
+	return (x+y);
+}
+	
+	public int sum (int x,int y,int z){
+		return (x+y+z);}
+	}
+public double sum(double x, double y){
+	return (x+Y);
+}
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
---
+++ github/src/github/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sdac
+ *
+ */
+package github;
